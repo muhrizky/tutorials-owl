61471880b70290eb4e83873c548a3d3e9c56125c
Géry Debongnie <ged@odoo.com>
2023-05-10 21:33:15 +0200

    [Solution] 2.6 Display a pie chart

--- awesome_dashboard/static/src/dashboard_item.js
+++ awesome_dashboard/static/src/dashboard_item.js
@@ -5,6 +5,7 @@ import { Component } from "@odoo/owl";
 export class DashboardItem extends Component {
     static template = "awesome_dashboard.DashboardItem"
     static props = {
+        size: {type: Number, optional: true},
         slots: {
             type: Object,
             shape: {
