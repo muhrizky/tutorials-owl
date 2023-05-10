24c0dda9b1462250d45926aaac0ed7a8ff84fa7e
Géry Debongnie <ged@odoo.com>
2023-05-10 17:27:36 +0200

    [Solution] 2.3 Add a DashboardItem

---
+++ awesome_dashboard/static/src/dashboard_item.js
@@ -0,0 +1,15 @@
+/** @odoo-module */
+
+import { Component } from "@odoo/owl";
+
+export class DashboardItem extends Component {
+    static template = "awesome_dashboard.DashboardItem"
+    static props = {
+        slots: {
+            type: Object,
+            shape: {
+                default: Object
+            }
+        }
+    };
+}
